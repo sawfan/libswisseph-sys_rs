ac68a1d75a8b1d3a5eb3303511c3cd587a124bd6
Sawfan <sawfan@sawfan.com>
2024-08-03 01:05:29 +0000

    Adds call to set ephemeris path in example

--- lib.rs
+++ lib.rs
@@ -7,10 +7,17 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::ffi::CString;
+    use std::os::raw::c_char;
 
     #[test]
     fn swe_calc_ut_works() {
         unsafe {
+            let s = "/users/ephe";
+            let c_str = CString::new(s).unwrap();
+            let path: *const c_char = c_str.as_ptr() as *const c_char;
+            swe_set_ephe_path(path);
+
             let mut xx: [f64; 6] = [0.0; 6];
             let mut serr = [0; AS_MAXCH as usize];
 
@@ -33,20 +40,28 @@ mod tests {
             let _lng = xx[0];
             let _lat = xx[1];
             let _speed = xx[3];
+
+            swe_close();
         }
     }
 
     #[test]
     fn swe_julday_works() {
-        let y = 2002;
-        let m = 1;
-        let d = 1;
-        let h = 0.0;
+        unsafe {
+            let s = "/users/ephe";
+            let c_str = CString::new(s).unwrap();
+            let path: *const c_char = c_str.as_ptr() as *const c_char;
+            swe_set_ephe_path(path);
 
-        let _jd = unsafe {
-            swe_julday(y, m, d, h, SE_GREG_CAL as i32)
+            let y = 2002;
+            let m = 1;
+            let d = 1;
+            let h = 0.0;
+
+            let _jd = swe_julday(y, m, d, h, SE_GREG_CAL as i32);
+
+            swe_close();
         };
-
     }
 
 //    #[test]
